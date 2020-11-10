d0491126d7fe2560747bcedb234948138be29712
Luis Papiernik <lpapiernik24@gmail.com>
2020-11-10 10:52:44 -0500

    test/feat: se agrega test e implementacion de la vecindad de Neumann

--- pycellslib/twodimensional/neighborhoods.py
+++ pycellslib/twodimensional/neighborhoods.py
@@ -65,4 +65,43 @@ class L2Neighborhood(Neighborhood):
 
 
 class NeumannNeighborhood(Neighborhood):
-    """docstring for NeumannNeighborhood"""
+    """
+    Esta clase representa la vecindad de Neumann de una celula en un espacio
+    2-dimensional
+
+    Params
+    ------
+    radius(int): radio de la vecindad
+    inclusive(bool): si es True, la celula afecta su propia transicion, False
+        en caso contrario
+    """
+
+    def __init__(self, radius=1, inclusive=True):
+        self.radius = radius
+        xx, yy = np.meshgrid(np.arange(-radius, radius + 1, 1, dtype=np.int),
+                             np.arange(-radius, radius + 1, 1, dtype=np.int),
+                             sparse=True)
+        self.mask = np.abs(xx) + np.abs(yy)
+        self.mask = self.mask <= radius
+        if not inclusive:
+            self.mask[radius, radius] = 0
+
+    def get_mask(self):
+        """
+        Este metodo retorna la mascara que define la vecindad de una celula
+
+        Returns
+        -------
+        out(ndarray(bool)): mascara
+        """
+        return self.mask
+
+    def get_offset(self):
+        """
+        Este metodo retorna el offset de la mascara
+
+        Returns
+        -------
+        out(int|tuple): valor que indica el offset en cada eje de la mascara
+        """
+        return -self.radius, -self.radius
--- tests/test_twodimensional/test_neighborhoods.py
+++ tests/test_twodimensional/test_neighborhoods.py
@@ -64,10 +64,6 @@ class TestCircularNeighborhood(unittest.TestCase):
         """
         Este metodo testea el metodo get_mask con radio 1
         """
-        neighborhood = MooreNeighborhood(1, inclusive=False)
-        mask = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]], dtype=np.bool)
-
-        self.assertTrue(np.array_equal(neighborhood.get_mask(), mask))
 
     def test_get_mask_case_2(self):
         """
@@ -120,18 +116,55 @@ class TestNeumannNeighborhood(unittest.TestCase):
         """
         Este metodo testea el metodo get_mask con radio 1
         """
+        neighborhood = NeumannNeighborhood(1, inclusive=False)
+        mask = np.array([[0, 1, 0], [1, 0, 1], [0, 1, 0]], dtype=np.bool)
+
+        self.assertTrue(np.array_equal(neighborhood.get_mask(), mask))
 
     def test_get_mask_case_2(self):
         """
         Este metodo testea el metodo get_mask con radio 4
         """
+        neighborhood = NeumannNeighborhood(2, inclusive=True)
+        mask = np.array([[0, 0, 1, 0, 0],
+                         [0, 1, 1, 1, 0],
+                         [1, 1, 1, 1, 1],
+                         [0, 1, 1, 1, 0],
+                         [0, 0, 1, 0, 0]], dtype=np.bool)
+
+        self.assertTrue(np.array_equal(neighborhood.get_mask(), mask))
+
+    def test_get_mask_case_3(self):
+        """
+        Este metodo testea el metodo get_mask con radio 4
+        """
+        neighborhood = NeumannNeighborhood(4, inclusive=False)
+        mask = np.array([[0, 0, 0, 0, 1, 0, 0, 0, 0],
+                         [0, 0, 0, 1, 1, 1, 0, 0, 0],
+                         [0, 0, 1, 1, 1, 1, 1, 0, 0],
+                         [0, 1, 1, 1, 1, 1, 1, 1, 0],
+                         [1, 1, 1, 1, 0, 1, 1, 1, 1],
+                         [0, 1, 1, 1, 1, 1, 1, 1, 0],
+                         [0, 0, 1, 1, 1, 1, 1, 0, 0],
+                         [0, 0, 0, 1, 1, 1, 0, 0, 0],
+                         [0, 0, 0, 0, 1, 0, 0, 0, 0]], dtype=np.bool)
+
+        self.assertTrue(np.array_equal(neighborhood.get_mask(), mask))
 
     def test_get_offset_case_1(self):
         """
         Este metodo testea el metodo get_offset con radio 1
         """
+        neighborhood = NeumannNeighborhood(1)
+        offset = (-1, -1)
+
+        self.assertEqual(neighborhood.get_offset(), offset)
 
     def test_get_offset_case_2(self):
         """
         Este metodo testea el metodo get_offset con radio 4
         """
+        neighborhood = NeumannNeighborhood(4)
+        offset = (-4, -4)
+
+        self.assertEqual(neighborhood.get_offset(), offset)
