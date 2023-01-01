0c8b9ebd7fe40f56d49a1fa05cf8d8a21e2ed62c
Luis Papiernik <lpapiernik24@gmail.com>
2023-01-01 14:39:12 -0500

    The FiniteNGridTopology class and the NGridIterator class were updated

---
+++ pycellslib/core/position_iterator.py
@@ -0,0 +1,38 @@
+from abc import ABCMeta, abstractmethod
+from typing import Tuple, Type
+
+import numpy as np
+import numpy.typing as npt
+
+
+class PositionIterator(ABCMeta):
+    @abstractmethod
+    def __iter__(self) -> Type["PositionIterator"]:
+        """"""
+
+    @abstractmethod
+    def __next__(self) -> Tuple[int, ...]:
+        """"""
+
+
+class NGridIterator(PositionIterator):
+    def __init__(
+        self, dimensions: npt.NDArray[np.int], frontiers_width: npt.NDArray[np.int]
+    ) -> None:
+        self.dimensions = dimensions
+        self.frontiers_width = frontiers_width
+        self.index = np.ndindex(*(dimensions + frontiers_width))
+
+    def __iter__(self) -> Type[PositionIterator]:
+        return self
+
+    def __next__(self) -> Tuple[int, ...]:
+        while True:
+            # se revisa que las coordenadas esten en el limite permitido
+            coordinate = np.array(next(self.index), dtype=int)
+
+            inferior_limit = self.frontiers_width - 1 < coordinate
+            superior_limit = coordinate < self.dimensions - self.frontiers_width
+
+            if np.all(inferior_limit & superior_limit):
+                return tuple(coordinate)
--- pycellslib/core/topology.py
+++ pycellslib/core/topology.py
@@ -1,10 +1,10 @@
 from abc import ABCMeta, abstractmethod
-from typing import Iterable, List, Optional, Tuple, Union
+from typing import Iterable, List, Optional, Tuple, Type, Union
 
 import numpy as np
 import numpy.typing as npt
 
-from pycellslib.utils import PositionIterator
+from pycellslib.core.position_iterator import NGridIterator
 
 
 class Topology(metaclass=ABCMeta):
@@ -29,15 +29,7 @@ class Topology(metaclass=ABCMeta):
     misma condicion para las vecindades)
     """
 
-    @abstractmethod
-    def get_offset(self):
-        """
-        Este metodo debe retornar el offset que se le hacen a las posiciones
-        en la matrix para no considerar las celulas de las fronteras en el
-        proceso de actualizacion
-        """
-
-    @abstractmethod
+    # @abstractmethod
     def __iter__(self):
         """
         La clase Topology debe brindar una interfaz por la cual se pueda
@@ -177,242 +169,234 @@ class Topology(metaclass=ABCMeta):
         """
 
 
-class FiniteNGridTopology(Topology):
-    """
-    La topologia representa la informacion espacial de una automata, esto es,
-    tiene encapsulada las dimensiones, la distribucion de las celulas, los
-    valores de estados y atributos en cada parte del espacio, metodos
-    que extraen y asignan valores a subregiones del espacio...
-
-    Esta clase representa una topologia rectangular n-dimensional finita, esto
-    es, para 2 dimensiones se puede visualizar como una teselacion de
-    rectangulos, para 3 dimensiones como una teselacion de cubos, ...
-
-    Parameters
-    ----------
-    attributes_number(int): numero de atributos de cada celula en el espacio
-    dimensions(tuple(int)|list(int)|ndarray(int)): dimensiones del espacio
-    border_widths(tuple(int)|list(int)|ndarray(int)): dimensiones de la
-        frontera. Estas dimensiones se le suman a las dimensiones del espacio
-    """
-
+class FiniteNGridTopology:
     def __init__(
         self,
         attributes_number: int,
         dimensions: Union[Iterable[int], npt.NDArray[np.int]],
-        border_widths: Union[Iterable[int], npt.NDArray[np.int]],
+        frontiers_width: Union[Iterable[int], npt.NDArray[np.int]],
+        memory_length: int,
     ) -> None:
-        # numero de atributos de cada celula en el espacio
-        self.attributes_number = attributes_number
-        # dimensiones del espacio sin tener en cuenta la frontera
         self.dimensions = np.array(dimensions, dtype=int)
-        # dimensiones de la frontera
-        self.border_widths = np.array(border_widths, dtype=int)
+        self.frontiers_width = np.array(frontiers_width, dtype=int)
+        self.sub_dimensions = self.dimensions - 2 * self.frontiers_width
 
-        # dimensiones reales del espacio, esto es, considerando la frontera
-        self.real_dimensions = self.dimensions + 2 * self.border_widths
-
-        # subregion del espacio completo, sin considerar la frontera
-        self.subshape = tuple(
-            slice(self.border_widths[i], self.dimensions[i] + self.border_widths[i])
-            for i in range(self.dimensions.size)
+        # The +1 in the second component stands for states, that is, index
+        # 0 is for states
+        self.cells = np.zeros(
+            (
+                memory_length,
+                attributes_number + 1,  # to save states along with attributes
+                *self.dimensions,
+            )
         )
 
-        # el indice 0 corresponde al buffer 1 y el indice 1 corresponde al
-        # buffer 2
-        self.states = [
-            np.zeros(self.real_dimensions, dtype=int),
-            np.zeros(self.real_dimensions, dtype=int),
-        ]
+        # Region without frontiers
+        not_frotier = tuple(
+            slice(self.frontiers_width[i], self.dimensions[i] - self.frontiers_width[i])
+            for i in range(self.dimensions.size)
+        )
+        self.frontier_mask = np.ones(self.dimensions, dtype=bool)
+        # el subshape no hace parte de la frontera
+        self.frontier_mask[not_frotier] = 0
 
-        # si se tienen 0 atributos, entonces no hace falta crear un array
-        self.attributes = []
-        if attributes_number != 0:
-            self.attributes = [
-                np.zeros((*self.real_dimensions, attributes_number), dtype=float),
-                np.zeros((*self.real_dimensions, attributes_number), dtype=float),
-            ]
+        self.attributes_number = attributes_number
+        self.memory_length = memory_length
 
-        # estos atributos llevan la cuenta de que buffer se usa para lectura y
-        # que buffer se usa para escritura
-        self.write_buffer = 0
-        self.read_buffer = 1
+        # This is the index for the writing board
+        self.writing_board = 0
+        # this is the index for the reading board
+        self.reading_board = memory_length - 1
 
-    def get_offset(self) -> npt.NDArray[np.int]:
-        """
-        Este metodo debe retornar el offset que se le hacen a las posiciones
-        en la matrix para no considerar las celulas de las fronteras en el
-        proceso de actualizacion
-        """
-        return self.border_widths
-
-    def __iter__(self) -> PositionIterator:
-        """
-        La clase Topology debe brindar una interfaz por la cual se pueda
-        iterar por cada celula para realizar el proceso de actualizacion, este
-        metodo retorna un iterador sobre las posiciones de las celulas que son
-        actualizables (esto es, las que no estan en la frontera)
-
-        Returns
-        -------
-        out(iter(list(tuple(int)))): iterador que recorre cada uno de los
-            indices de las celulas que son actualizables
-        """
-        return PositionIterator(self.dimensions, self.border_widths)
+    def __iter__(self) -> Type[NGridIterator]:
+        return NGridIterator(self.dimensions, self.frontiers_width)
 
     def flip(self) -> None:
-        """
-        Este metodo cambia el papel (ser de lectura o ser de escritura) que
-        cumplen las 2 estructuras de datos en las que se almacenan la
-        informacion de estados y atributos de las celulas
-        """
-        self.write_buffer += 1
-        self.read_buffer += 1
+        self.writing_board += 1
+        self.writing_board %= self.memory_length
 
-        self.write_buffer %= 2
-        self.read_buffer %= 2
+        self.reading_board += 1
+        self.reading_board %= self.memory_length
 
     def get_cell(
-        self, position: Tuple[int]
-    ) -> Tuple[int, Optional[npt.NDArray[np.float]]]:
-        """
-        Este metodo obtiene la informacion de una celula, tanto los estados
-        como los atributos. Este metodo no permite obtener una celula que esta
-        en la frontera
+        self, position: Tuple[int, ...], board: int = None
+    ) -> npt.NDArray[np.float]:
+        board = board or self.reading_board
+        return self.cells[board][(slice(None, None, None), *position)]
 
-        Parameters
-        ----------
-        position(tuple(int)): representan la posicion de la celula
+    def get_cells(
+        self, subregion: Iterable[Tuple[int, int]] = None, board: int = None
+    ) -> npt.NDArray[np.float]:
+        board = board or self.reading_board
+        cells = self.cells[board]
+        if subregion is not None:
+            index = [slice(i, f) for i, f in subregion]
+            cells = self.cells[board][(slice(None, None, None), *index)]
+        return cells
 
-        Returns
-        -------
-        outs(tuple): tupla cuya primera componente es un entero con el valor
-            del estado de la celula asociada a la posicion dada, y la segunda
-            componente es un array con el valor de los atributos, o None, en
-            caso de que las celulas no tenga atributos
-        """
-        state = self.states[self.read_buffer][position]
+    def get_states(
+        self, board: int = None, with_frontiers: bool = False
+    ) -> npt.NDArray[np.float]:
+        board = board or self.reading_board
+        states = self.cells[board][0]
+        if not with_frontiers:
+            states = states[~self.frontier_mask].reshape(self.sub_dimensions)
+        return states
 
-        attributes = None
-        if self.attributes is not None:
-            attributes = self.attributes[self.read_buffer][position]
+    def get_attributes(
+        self, board: int = None, with_frontiers: bool = False
+    ) -> npt.NDArray[np.float]:
+        board = board or self.reading_board
+        attributes = self.cells[board][1:]
+        if not with_frontiers:
+            attributes = attributes[:, ~self.frontier_mask].reshape(
+                (self.attributes_number, *self.sub_dimensions)
+            )
+        return attributes
 
-        return state, attributes
-
-    def get_states(self) -> npt.NDArray[np.int]:
-        """
-        Este metodo retorna los estados de las celulas, no se tiene en cuenta
-        la frontera
-
-        Returns
-        -------
-        out(ndarray(int)): estados de las celulas
-        """
-        return self.states[self.read_buffer][self.subshape]
-
-    def get_attributes(self) -> npt.NDArray[np.float]:
-        """
-        Este metodo retorna los atributos de las celulas, no se tiene en cuenta
-        la frontera
-
-        Returns
-        -------
-        out(ndarray(float)): atributos de las celulas
-        """
-        return self.attributes[self.read_buffer][self.subshape]
-
-    def update_cell(
+    def set_cell_value(
         self,
-        position: Tuple[int, int],
+        position: Tuple[int, ...],
         cell_state: int,
         cell_attributes: Optional[Union[List[float], npt.NDArray[np.float]]],
+        board: int = None,
     ) -> None:
-        """
-        Este metodo actualiza la informacion de una celula, tanto estados como
-        atributos
+        board = board or self.writing_board
+        self.cells[board][(slice(None, None, None), *position)] = [
+            cell_state,
+            *cell_attributes,
+        ]
 
-        Parameters
-        ----------
-        position(tuple(int)): representa la posicion de la celula que sera
-            actualizada
-        cell_state(int): entero con el valor del estado de la celula
-        cell_attributes(list(float)|ndarray(float)|None): lista o arreglo con
-            los valores de los atributos. Si las celulas no tienen atributos
-            se pasa None
-        """
-        self.states[self.write_buffer][position] = cell_state
-
-        if self.attributes is not None:
-            self.attributes[self.write_buffer][position] = cell_attributes
-
-    def set_border_values(
+    def set_border_cell_values(
         self,
         cell_state: int,
-        cell_attributes: Optional[Union[List[float], npt.NDArray[np.float]]] = None,
+        cell_attributes: Optional[Union[List[float], npt.NDArray[np.float]]],
+        board: int = None,
     ) -> None:
-        """
-        Este metodo establece el valor en los bordes
+        board = board or self.writing_board
+        broadcast_dimension = (np.sum(self.frontier_mask), 1 + self.attributes_number)
+        self.cells[board][:, self.frontier_mask] = np.broadcast_to(
+            [cell_state, *cell_attributes], broadcast_dimension
+        ).transpose()
 
-        Parameters
-        ----------
-        cell_state(int): especifica el valor de los estados en los bordes
-        cell_attributes(list(float)|ndarray(float)|None): especifica el valor
-            de los atributos en los bordes, cada elemento de la lista
-            especifica un atributo
-        """
-        # se crea mascara para establecer el valor en los bordes
-        mask = np.ones(self.real_dimensions, dtype=bool)
-        # el subshape no hace parte de la frontera
-        mask[self.subshape] = 0
-        self.states[self.write_buffer][mask] = cell_state
-
-        if self.attributes is not None:
-            self.attributes[self.write_buffer][mask] = cell_attributes
-
-    def set_values_from(
+    def set_cells_values(
         self,
         cell_state: int,
-        cell_attributes: Optional[Union[List[float], npt.NDArray[np.float]]] = None,
+        cell_attributes: Optional[Union[List[float], npt.NDArray[np.float]]],
+        subregion: Iterable[Tuple[int, int]] = None,
+        board: int = None,
     ) -> None:
-        """
-        Este metodo establece el valor de las celulas usando los mismos
-        parametros tanto para los estados, como para los atributos
+        board = board or self.writing_board
 
-        Parameters
-        ----------
-        cell_state(int): entero con el valor de los estados de las celulas
-        cell_attributes(list(float)|ndarray(float)|None): especifica el valor
-            de los atributos, si la celula no tiene atributos se pasa None
-        """
-        self.states[self.write_buffer][self.subshape] = cell_state
+        index = slice(None, None, None)
+        dimensions = self.dimensions
+        if subregion is not None:
+            index = [slice(None, None, None)]
+            index.extend([slice(i, f) for i, f in subregion])
+            index = tuple(index)
+            dimensions = [f - i for i, f in subregion]
 
-        if self.attributes is not None:
-            self.attributes[self.write_buffer][self.subshape] = cell_attributes
+        output_shape = self.attributes_number + 1, *dimensions
+        new_values = [cell_state, *cell_attributes]
+        broadcast_dimension = np.product(dimensions), 1 + self.attributes_number
 
-    def set_values_from_configuration(
+        self.cells[board][index] = (
+            np.broadcast_to(new_values, broadcast_dimension)
+            .transpose()
+            .reshape(*output_shape)
+        )
+
+    def set_cells_state_values(
         self,
-        cell_states: npt.NDArray[np.int],
-        cell_attributes: Optional[npt.NDArray[np.float]] = None,
+        cell_state: int,
+        subregion: Iterable[Tuple[int, int]] = None,
+        board: int = None,
     ) -> None:
-        """
-        Este metodo establece el valor de las celulas desde un arreglo de
-        estados y un arreglo de atributos
+        board = board or self.writing_board
 
-        Parameters
-        ----------
-        cell_states(ndarray(int)): arreglo con los valores de los estados de
-            cada celula
-        cell_attributes(ndarray(float)|None): arreglo con los valores de los
-            atributos de cada celula. Si las celulas no tienen atributos se
-            pasa None
-        """
-        self.states[self.write_buffer][self.subshape] = cell_states
+        index = slice(0, 1)
+        if subregion is not None:
+            index = [slice(0, 1)]
+            index.extend([slice(i, f) for i, f in subregion])
+            index = tuple(index)
 
-        if self.attributes is not None:
-            self.attributes[self.write_buffer][self.subshape] = cell_attributes
+        self.cells[board][index] = cell_state
+
+    def set_cells_attributes_values(
+        self,
+        cell_attributes: Optional[Union[List[float], npt.NDArray[np.float]]],
+        subregion: Iterable[Tuple[int, int]] = None,
+        board: int = None,
+    ) -> None:
+        board = board or self.writing_board
+
+        index = slice(1, None)
+        dimensions = self.dimensions
+        if subregion is not None:
+            index = [slice(1, None)]
+            index.extend([slice(i, f) for i, f in subregion])
+            index = tuple(index)
+            dimensions = [f - i for i, f in subregion]
+
+        output_shape = self.attributes_number, *dimensions
+        broadcast_dimension = np.product(dimensions), self.attributes_number
+
+        self.cells[board][index] = (
+            np.broadcast_to(cell_attributes, broadcast_dimension)
+            .transpose()
+            .reshape(*output_shape)
+        )
+
+    def set_cells_states_from_array(
+        self,
+        data: npt.NDArray[np.float],
+        subregion: Iterable[Tuple[int, int]] = None,
+        board: int = None,
+    ) -> None:
+        board = board or self.writing_board
+
+        index = slice(0, 1)
+        if subregion is not None:
+            index = [slice(0, 1)]
+            index.extend([slice(i, f) for i, f in subregion])
+            index = tuple(index)
+
+        self.cells[board][index] = data
+
+    def set_cells_attributes_from_array(
+        self,
+        data: npt.NDArray[np.float],
+        subregion: Iterable[Tuple[int, int]] = None,
+        board: int = None,
+    ) -> None:
+        board = board or self.writing_board
+
+        index = slice(1, None, None)
+        if subregion is not None:
+            index = [slice(1, None, None)]
+            index.extend([slice(i, f) for i, f in subregion])
+            index = tuple(index)
+
+        self.cells[board][index] = data
+
+    def set_cells_from_array(
+        self,
+        data: npt.NDArray[np.float],
+        subregion: Iterable[Tuple[int, int]] = None,
+        board: int = None,
+    ) -> None:
+        board = board or self.writing_board
+
+        index = slice(None, None, None)
+        if subregion is not None:
+            index = [slice(None, None, None)]
+            index.extend([slice(i, f) for i, f in subregion])
+            index = tuple(index)
+
+        self.cells[board][index] = data
 
     def apply_mask(
-        self, position: Tuple[int, int], mask: npt.NDArray[np.int]
+        self, position: Tuple[int, ...], mask: npt.NDArray[np.int]
     ) -> Tuple[npt.NDArray[np.int], npt.NDArray[np.float]]:
         """
         Este metodo retorna la vecindad de una celula mediante la aplicacion
